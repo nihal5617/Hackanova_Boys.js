97c096d6a5723e7784b327cfe82a47dd91197c51
Deadpool <abcd162003@gmail.com>
2023-01-20 23:34:19 +0530

    added images in farmer

--- FARMERS/src/constants/images.js
+++ FARMERS/src/constants/images.js
@@ -1,5 +1,19 @@
 import farmarea from '../assets/farmarea.png';
+import background from '../assets/Background_app.jpg';
+import Internetaccess from '../assets/Internet_access_error.png';
+import Logoonboard from '../assets/logo_onboard.png';
+import logo from '../assets/logo.jpg';
+import Mindmapadmin from '../assets/Mindmap_Admin.jpeg';
+import Mindmapexpert from '../assets/Mindmap_Expert.jpeg';
+import Mindmapfarmer from '../assets/Mindmap_Farmer.jpeg';
 
 export default {
-    farmarea
+    farmarea,
+    background,
+    Internetaccess,
+    Logoonboard,
+    logo,
+    Mindmapadmin,
+    Mindmapexpert,
+    Mindmapfarmer,
 }
