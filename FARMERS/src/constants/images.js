8ec62b666e8aae5af85d75fa8b57e025db67d818
darshm1029 <darshm1029@gmail.com>
2023-01-21 01:24:22 +0530

    images uploaded

--- FARMERS/src/constants/images.js
+++ FARMERS/src/constants/images.js
@@ -6,6 +6,10 @@ import logo from '../assets/logo.jpg';
 import Mindmapadmin from '../assets/Mindmap_Admin.jpeg';
 import Mindmapexpert from '../assets/Mindmap_Expert.jpeg';
 import Mindmapfarmer from '../assets/Mindmap_Farmer.jpeg';
+import icon1 from '../assets/icon1.png';
+import icon2 from '../assets/icon2.png';
+import icon3 from '../assets/icon3.png';
+import icon4 from '../assets/icon4.png';
 
 export default {
     farmarea,
@@ -16,4 +20,8 @@ export default {
     Mindmapadmin,
     Mindmapexpert,
     Mindmapfarmer,
+    icon1,
+    icon2,
+    icon3,
+    icon4,
 }
