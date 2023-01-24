614e1f2fc5a3fc753ea971ee316c387ba8c3e97a
Nihal Vishal Gupta <nihalng786@gmail.com>
2023-01-24 19:49:59 +0530

    last commit

--- CropShield/src/screens/main/screens/forum/ViewPost.js
+++ CropShield/src/screens/main/screens/forum/ViewPost.js
@@ -57,7 +57,7 @@ export default function ViewPost({navigation}) {
     navigation.setOptions({
       header: () => (
         <Header
-          title={'Item.Title'}
+          title={'Potato Late Blight'}
           showBackButton={true}
           navigation={navigation}
         />
